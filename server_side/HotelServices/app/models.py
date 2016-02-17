e2de364b3d27a3013064a198efcde6b36bfe43a9
Waqar <waqar@WikiLeaks.local>
2016-02-17 10:47:41 +0500

    delete unnecessary files

--- server_side/HotelServices/app/models.py
+++ server_side/HotelServices/app/models.py
@@ -1,7 +1,7 @@
 __author__ = 'waqar'
 
-from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 class Permission(db.Model):
@@ -16,6 +16,9 @@ class Permission(db.Model):
 
 
 class User(db.Model):
+    """
+
+    """
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
